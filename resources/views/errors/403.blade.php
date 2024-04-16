8af435d15e610a95072d6201908a247b27989f3e
Awfa Abou Assali <a.abouassali@staff.ososs.com>
2024-04-16 13:45:25 +0200

    Create 404 and 403 Page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,31 @@
+@extends('admin.admin_dashboard')
+@section('admin')
+    <!-- wrapper -->
+    <div class="wrapper">
+    
+        <div class="error-404 d-flex align-items-center justify-content-center">
+            <div class="container">
+                <div class="card py-5">
+                    <div class="row g-0">
+                        <div class="col col-xl-5">
+                            <div class="card-body p-4">
+                                <h1 class="display-1"><span class="text-primary">4</span><span
+                                        class="text-danger">0</span><span class="text-success">3</span></h1>
+                                <h2 class="font-weight-bold display-4">Lost in Space</h2>
+                                <p>USER DOES NOT HAVE THE RIGHT PERMISSIONS.</p>
+                                
+                            </div>
+                        </div>
+                        <div class="col-xl-7">
+                            <img src="https://cdn.searchenginejournal.com/wp-content/uploads/2019/03/shutterstock_1338315902.png"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <!--end row-->
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    <!-- end wrapper -->
+@endsection
